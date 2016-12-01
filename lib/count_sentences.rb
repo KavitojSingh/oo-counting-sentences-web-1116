c23e95dff63ec8f1ca1272a4845192a5e84ec3ff
github username <github email address>
2016-12-01 14:06:59 -0500

    OO Counting Sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+  	self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
   def count_sentences
-
+  	new_string = self.gsub("?", ".")
+	second_string = new_string.gsub("!", ".")
+	first_array = second_string.split(".")
+	second_array = first_array - [""]
+	second_array.count
   end
 end
+
